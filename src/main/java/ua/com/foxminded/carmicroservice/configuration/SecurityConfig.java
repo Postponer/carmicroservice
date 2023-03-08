6070410137b6881ff0e7a26b48debfac571703fa
Penkovsky Mykola <istonosha@gmail.com>
2023-03-08 14:59:59 +0200

    adjusted PR

--- src/main/java/ua/com/foxminded/carmicroservice/configuration/SecurityConfig.java
+++ src/main/java/ua/com/foxminded/carmicroservice/configuration/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig {
 				.authorizeHttpRequests()
 				.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
 				.requestMatchers(HttpMethod.POST, "/api/**").authenticated()
+				.requestMatchers(HttpMethod.PUT, "/api/**").authenticated()
+				.requestMatchers(HttpMethod.DELETE, "/api/**").authenticated()
 				.anyRequest().permitAll()
 				.and()
 				.oauth2Login()
